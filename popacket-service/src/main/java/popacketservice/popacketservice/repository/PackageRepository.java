9a69589f551fe79fb732cf2fae3328c8350a09d9
Francisco Valverde <julval_09@hotmail.com>
2024-06-03 20:21:30 -0500

    Solucion de errores

--- popacket-service/src/main/java/popacketservice/popacketservice/repository/PackageRepository.java
+++ popacket-service/src/main/java/popacketservice/popacketservice/repository/PackageRepository.java
@@ -4,6 +4,7 @@ package popacketservice.popacketservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import popacketservice.popacketservice.model.entity.Package;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
