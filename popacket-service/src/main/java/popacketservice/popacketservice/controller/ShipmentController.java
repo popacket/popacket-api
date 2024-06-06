fe638ad57bb6a23efe0f94fb9042f4a4632b8f98
Izpo17F <ponce.1714.frp@gmail.com>
2024-06-06 00:22:27 -0500

    Solucion de historia de usuario cancelShipment

--- popacket-service/src/main/java/popacketservice/popacketservice/controller/ShipmentController.java
+++ popacket-service/src/main/java/popacketservice/popacketservice/controller/ShipmentController.java
@@ -3,6 +3,7 @@ package popacketservice.popacketservice.controller;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,16 +14,16 @@ import popacketservice.popacketservice.model.dto.ShipmentResponseDTO;
 import popacketservice.popacketservice.service.ShipmentService;
 
 @RestController
-@RequestMapping
+@RequestMapping("/shipments")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class ShipmentController {
-
+    @Autowired
     private ShipmentService shipmentService;
 
-    @PostMapping("/shipments/cancel/{id}")
+    @PostMapping("/cancel/{id}")
     public ResponseEntity<ShipmentResponseDTO> cancelShipment(@PathVariable("id") Long shipmentId) {
         ShipmentResponseDTO shipment = shipmentService.cancelShipmentById(shipmentId);
         return new ResponseEntity<>(shipment, HttpStatus.OK);
