75bf695c420da564be7f4f5b7d98e3eb7aa54578
Izpo17F <ponce.1714.frp@gmail.com>
2024-06-20 23:40:06 -0500

    mejoramiento del metodo obtener costo

--- popacket-service/src/main/java/popacketservice/popacketservice/service/ShipmentService.java
+++ popacket-service/src/main/java/popacketservice/popacketservice/service/ShipmentService.java
@@ -50,7 +50,7 @@ public class ShipmentService {
     public Double getShipmentCost(Double weight, String serviceType) {
         BigDecimal priceBase = shipmentRateRepository.getBasePrice(BigDecimal.valueOf(weight), serviceType);
         BigDecimal pricePerKilometer = shipmentRateRepository.getPricePerKilometer(BigDecimal.valueOf(weight), serviceType);
-        Double price = priceBase.multiply(BigDecimal.valueOf(weight)).add(pricePerKilometer).doubleValue();
+        Double price = pricePerKilometer.multiply(BigDecimal.valueOf(weight)).add(priceBase).doubleValue();
         return price;
     }
 
