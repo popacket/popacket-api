c70ba0ee3f4ca78ba468797ef231ef300c4e2009
PabloAsmad15 <pabloasmad15@gmail.com>
2024-06-22 20:45:10 -0500

    Actualizacion de la rama RescheduleShipment

--- popacket-service/src/main/java/popacketservice/popacketservice/service/ShipmentService.java
+++ popacket-service/src/main/java/popacketservice/popacketservice/service/ShipmentService.java
@@ -92,4 +92,13 @@ public class ShipmentService {
 
             return shipmentMapper.convertToDTO(savedShipment);}
     }
+
+    public ShipmentResponseDTO updateScheduleShipment(ShipmentRequestDTO shipmentRequestDTO) {
+        Shipment shipment = shipmentMapper.convertToEntity(shipmentRequestDTO);
+        Shipment shipmentTemp = shipmentRepository.getShipmentById(shipment.getId()).orElseThrow();
+        shipmentTemp.setPickupDateTime(shipment.getPickupDateTime());
+        shipmentTemp.setDeliveryDateTime(shipment.getPickupDateTime().plusDays(3));
+        shipmentRepository.save(shipmentTemp);
+        return shipmentMapper.convertToDTO(shipmentTemp);
+    }
 }
