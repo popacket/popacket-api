a2890225f0e253df7caf14e4dba8aafa379bb56a
Fabricio Ruiz Ponce <ponce.1714.frp@gmail.com>
2024-07-03 16:33:14 -0500

    Actualizacion del main 1.2 Creacion del mapper ShippingRate

---
+++ popacket-service/src/main/java/popacketservice/popacketservice/mapper/ShippingRateMapper.java
@@ -0,0 +1,30 @@
+package popacketservice.popacketservice.mapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+import popacketservice.popacketservice.model.dto.ShippingRateResponseDTO;
+import popacketservice.popacketservice.model.entity.ShippingRate;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+@Data
+public class ShippingRateMapper {
+
+    private final ModelMapper modelMapper;
+
+    public ShippingRate convertToEntity(ShippingRateResponseDTO shippingRateResponseDTO){
+        return modelMapper.map(shippingRateResponseDTO, ShippingRate.class);
+    }
+    public ShippingRateResponseDTO convertToDto(ShippingRate shippingRate){
+        return modelMapper.map(shippingRate, ShippingRateResponseDTO.class);
+    }
+    public List<ShippingRateResponseDTO> convertToDto(List<ShippingRate> shippingRates){
+        return shippingRates.stream()
+                .map(this::convertToDto)
+                .toList();
+    }
+}
