a94dfff0861b3a5c92b22ce5b69d43b6c393ac3a
Fabricio Ruiz Ponce <ponce.1714.frp@gmail.com>
2024-07-10 23:36:19 -0500

    Creacion de la rama MasterIzpo

--- popacket-service/src/main/java/popacketservice/popacketservice/controller/ShipmentController.java
+++ popacket-service/src/main/java/popacketservice/popacketservice/controller/ShipmentController.java
@@ -39,13 +39,6 @@ public class ShipmentController {
         Double price = shipmentService.getShipmentCost(weight, serviceType);
         return new ResponseEntity<>(price, HttpStatus.OK);
     }
-
-    @GetMapping("/tracking_2/{id}")
-    public ResponseEntity<Object[]> getTrackingOb(@PathVariable("id") Long shipmentId) {
-        //ShipmentResponseDTO shipment = shipmentService.getShipmentById(shipmentId);
-        Object[] shipment = shipmentService.getStatusShipmentById(shipmentId);
-        return new ResponseEntity<>(shipment, HttpStatus.OK);
-    }
     @PutMapping("/makeShipment")
     public ResponseEntity<ShipmentResponseDTO> makeShipment(@RequestBody ShipmentRequestDTO shipmentDTO) {
         ShipmentResponseDTO shipmentResponseDTO = shipmentService.makeShipment(shipmentDTO);
